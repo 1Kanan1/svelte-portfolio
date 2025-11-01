d1366fe71e470e6f5210a8a63e0be7abf890c38f
1Kanan1 <83110215+1Kanan1@users.noreply.github.com>
2025-11-01 15:21:57 +0400

    feat: add new Navbar component with smooth scrool into view

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,12 @@
   import "./app.css";
   import { ModeWatcher } from "mode-watcher";
   import Experience from "$lib/components/Experience.svelte";
-  import ThemeToggle from "$lib/components/ui/ThemeToggle.svelte";
   import Skills from "$lib/components/Skills.svelte";
+  import Navbar from "$lib/components/Navbar.svelte";
 </script>
 
 <ModeWatcher />
-<header
-  class="fixed top-4 left-0 right-0 p-2 mx-auto md:max-w-xl bg-background border border-border rounded-xl z-1"
->
-  <ThemeToggle />
-</header>
+<Navbar />
 <div class="h-24"></div>
 <main class="max-w-screen z-0">
   <section class="grid mx-auto md:max-w-3xl gap-3">
---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import ThemeToggle from "./ui/ThemeToggle.svelte";
+
+  function scrollIntoView(event: MouseEvent) {
+    event.preventDefault(); // prevents from updating the URL
+    const target = event.currentTarget as HTMLAnchorElement;
+    const href = target.getAttribute("href");
+    if (!href) return;
+
+    const element = document.querySelector<HTMLElement>(href);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
+  const sections = ["Skills", "Experience"];
+</script>
+
+<header
+  class="fixed top-4 left-0 right-0 flex items-center justify-between px-4 py-2 mx-4 sm:mx-auto sm:max-w-xl bg-background border border-border rounded-xl z-1"
+>
+  <div class="flex gap-2">
+    {#each sections as section}
+      <a
+        href={`#${section.toLowerCase()}`}
+        onclick={scrollIntoView}
+        class="md:rounded-sm sm:px-2 sm:py-1 md:transition md:duration-300 md:hover:bg-accent"
+        >{section}</a
+      >
+    {/each}
+  </div>
+  <ThemeToggle />
+</header>
